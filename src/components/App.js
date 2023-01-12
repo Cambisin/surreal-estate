74c52e680d7da97cf14a602fb8725bf1a2edaa69
Cambisin <cameron_ewart17@hotmail.com>
2023-01-12 17:27:40 +0000

    readme and setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
-
+import React from 'react';
 import '../styles/app.css';
 
 function App() {
   return (
+    // eslint-disable-next-line react/react-in-jsx-scope
     <div className="App">
       <header className="App-header">
         <h2>Surreal Estate</h2>
